e46a96251bb903117c74035e9111092c82ad9640
lacey <hiu@example.com>
2019-02-27 16:24:05 +0000

    add first two queries

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+
+import java.util.List;
+
+public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+
+
+
+}
